62b9b1899b160a1c3c1ee2a14454ae397c4a01f1
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2024-03-23 20:24:42 -0400

    updated commented documents in files

--- src/database.cpp
+++ src/database.cpp
@@ -39,6 +39,24 @@ void DataBase::List() const
     }
 };
 
+/*
+    WHY WE USE BINARY SEARCH HERE
+    for the FindById algorithm, binary search is the perfect and most efficient way to find a user by their id.
+    The reason is because the user constructor uses a static count each time a user is created and that static value will be assigned to the users ID.
+
+    what does this mean?
+    Users should be inserted into the data base as soon as they are created,
+    this can change so that creating the user will push the user into the database through the user constructor.
+    The problem with that approach however is that the db has to be passed by REFERENCE to each user object. so say we are on a x86-64 bit architechture. This means that
+    each variable being passed by reference is 8 bytes. This seems feasible compared to the REAL size of the db object however, if there are lets say, 4 million users, then that means
+    that there will be 32 million bytes being used just from the User class constructors alone. This is a tremendous fault in the design and needs to be taken into account.
+    However, currently we are not passing any references to the constructor, so the size goes down from 32 million bytes to 0. Now back to the algorithm. The only way for this to work for now is to insert
+    users in order from which they are created into the db. In the future, I can maybe keep track of the inserts and sort as we insert which would be an interesting feature to add.
+
+
+
+*/
+
 User *DataBase::FindById(int id)
 {
     int left = 0;
